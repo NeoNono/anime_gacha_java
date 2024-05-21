ced41a36aa01511727efcf7c95702d68829ac1fa
Yuliia Shkekina <Y.Shkekina@student.tudelft.nl>
2024-05-21 17:36:47 +0300

    basic setup

--- app/src/main/java/oop_poliheh/controllers/PlayerController.java
+++ app/src/main/java/oop_poliheh/controllers/PlayerController.java
@@ -1,6 +1,15 @@
 package oop_poliheh.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PlayerController {
 
-//    @RestController
+    @GetMapping(path = {"", "/"})
+    public ResponseEntity<String> getMethod() {
+        return ResponseEntity.ok("Hello world");
+    }
+
 }
