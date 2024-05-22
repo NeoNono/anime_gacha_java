adfba94b08bed0e6301aadbc6d3660695a892356
Yuliia Shkekina <Y.Shkekina@student.tudelft.nl>
2024-05-22 17:26:52 +0300

    temporary stuff

--- commons/src/main/java/commons/Character.java
+++ commons/src/main/java/commons/Character.java
@@ -7,12 +7,20 @@ import javax.persistence.Id;
 
 
 //@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
-//        property = "id")
+ //       property = "id")
 @Entity
 public class Character {
     @Id
-    public long id;
+    public long code;
+    public String rarity;
+    public int health;
+    public int stamina;
+    public int damage;
+    public int price;
 
     public Character() {
+        int sellPrice = (int) (this.price*0.65);
     }
 }
+
+
--- commons/src/main/java/commons/OwnedCharacter.java
+++ commons/src/main/java/commons/OwnedCharacter.java
@@ -1,5 +1,60 @@
 package commons;
 
-public class OwnedCharacter {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.util.Objects;
 
+
+@Entity
+public class OwnedCharacter {
+    @Id
+    public long id;
+    @Id
+    public long code;
+
+
+    @ManyToOne
+    @JoinColumn(name = "code", referencedColumnName = "code")
+    private Character character;
+
+    @ManyToOne
+    @JoinColumn(name = "id",referencedColumnName = "id")
+    private Player player;
+
+    public OwnedCharacter() {
+        //for object mappers
+    }
+
+    public OwnedCharacter(long id, long code, Character character, Player player) {
+        this.id = id;
+        this.code = code;
+        this.character = character;
+        this.player = player;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OwnedCharacter that = (OwnedCharacter) o;
+        return id == that.id && code == that.code && Objects.equals(character, that.character)
+                && Objects.equals(player, that.player);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, code, character, player);
+    }
+
+    @Override
+    public String toString() {
+        return "OwnedCharacter{" +
+                "id=" + id +
+                ", code=" + code +
+                ", character=" + character +
+                ", player=" + player +
+                '}';
+    }
 }
