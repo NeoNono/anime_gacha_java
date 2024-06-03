cffaff815862b43e3a7b4c0e901960082bca4cde
Yuliia Shkekina <Y.Shkekina@student.tudelft.nl>
2024-06-04 02:32:33 +0300

    roulette service pt2

--- app/src/main/java/server/service/RouletteService.java
+++ app/src/main/java/server/service/RouletteService.java
@@ -1,8 +1,8 @@
 package server.service;
 
 import commons.Character;
-import commons.Fight;
 import commons.OwnedCharacter;
+import commons.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import server.database.CharacterRepository;
@@ -13,8 +13,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
-import static commons.Rarity.*;
-
 @Service
 public class RouletteService {
 
@@ -24,40 +22,60 @@ public class RouletteService {
 
     private final CharacterRepository characterRepository;
 
+    private final CharacterService characterService;
     @Autowired
-    public RouletteService(OwnedCharacterRepository ownedCharacterRepository,
-                           PlayerRepository playerRepository, CharacterRepository characterRepository) {
+    public RouletteService(OwnedCharacterRepository ownedCharacterRepository, PlayerRepository playerRepository,
+                           CharacterRepository characterRepository, CharacterService characterService) {
         this.ownedCharacterRepository = ownedCharacterRepository;
         this.playerRepository = playerRepository;
         this.characterRepository = characterRepository;
+        this.characterService = characterService;
     }
 
-    public Optional<Character> pullRoulette(long playerId) {
+
+
+
+    public Optional<List<OwnedCharacter>> pullRoulette(long id) {
+        Player player = playerRepository.findById(id).orElseThrow();
+        List<OwnedCharacter> ownedCharacters = ownedCharacterRepository.findAllByOwnedCharacterIdPlayerId(id);
+        if (player.getBalance() < 50) {
+            throw new IllegalStateException("This is your default character, it can't be sold!");
+        }
         Random rng = new Random();
         int roll = rng.nextInt(100);
-        Optional<Character> character = Optional.empty();
-        if (roll < 5) {
-            // character = getRandomLegendary();
-            // if (character.isEmpty()) {
-                    // player.addBalance(500);
-            //      return character;
-            // }
-            // else {
-            //     return character;
-        } else if(roll<25){
-            // character = getRandomRare();
+
+        Optional<Character> character;
+        if (roll <= 5) {
+            character = getRandomLegendary(ownedCharacters);
+        }
+        else if (roll <=30){
+            character = getRandomRare(ownedCharacters);
+        }
+        else
+            character = getRandomRegular(ownedCharacters);
+        //duplicates
+        if (character.isEmpty()) {
+            player.setBalance(player.getBalance() + 300);
+            playerRepository.save(player);
+            return Optional.empty();
         } else {
-            // character = getRandomRegular();
+            return Optional.of(characterService.addCharacterToPlayer(id, character.get().code));
         }
     }
 
-    private Optional<Character>getRandomLegendary(){
+    private static Optional<Character> getRandomLegendary (List<OwnedCharacter> ownedCharacters) {
+
 
     }
 
+    private static Optional<Character> getRandomRare (List<OwnedCharacter> ownedCharacters) {
 
+    }
 
+    private static Optional<Character> getRandomRegular (List<OwnedCharacter> ownedCharacters) {
 
+    }
 
+    //addCharacterToPlayer by id
 
 }
